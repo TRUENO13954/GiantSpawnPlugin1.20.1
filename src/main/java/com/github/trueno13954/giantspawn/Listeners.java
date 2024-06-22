3a6cfb87a1a33633367051e47a8f3e0ad65559d9
TRUENO13954 <144592965+TRUENO13954@users.noreply.github.com>
2024-06-22 18:02:48 +0900

    Update Listeners.java

--- src/main/java/com/github/trueno13954/giantspawn/Listeners.java
+++ src/main/java/com/github/trueno13954/giantspawn/Listeners.java
@@ -5,6 +5,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class Listeners implements Listener {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int num = rand.nextInt(50) + 100;
+        System.out.println(num);
+    }
 
     @EventHandler
     public void onSpawnZombie(EntitySpawnEvent e){}
